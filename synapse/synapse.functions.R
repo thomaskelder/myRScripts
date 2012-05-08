7b8fe23730a6d846fe9a783167312c92a6c062e9
Thomas Kelder <thomas@thomas-laptop.(none)>
2012-05-08 13:34:22 +0200

    added synapse script

---
+++ synapse/synapse.functions.R
@@ -0,0 +1,14 @@
+############################################
+## Utility functions for Synapse R client ##
+############################################
+createOrGetEntity = function(entity) {
+  name = properties(entity)$name
+  id = synapseQuery(paste("select id from entity where name == '", name, "'", sep=''))
+  
+  if(is.null(id)) { #Doesn't exist yet, create
+    entity = createEntity(entity)
+  } else { #Already exists, get
+    entity = getEntity(as.character(id))
+  }
+  entity
+}
