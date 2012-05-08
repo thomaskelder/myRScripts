bebc1c2c406a289545e464fe1503efec57c2262f
Thomas Kelder <thomas@thomas-laptop.(none)>
2012-05-08 13:33:04 +0200

    added network analysis scripts

---
+++ network-analysis/igraph.functions.R
@@ -0,0 +1,185 @@
+##########################################
+## Functions dealing with igraph graphs ##
+##                                      ##
+## Thomas Kelder, 2012                  ##
+##########################################
+
+###############################################################
+## Export igraph graph to GML format compatible to Cytoscape ##
+###############################################################
+saveGML = function(g, fileName, title = "untitled") {
+	attrToString = function(x) {
+		m = mode(x)
+		if(m == "numeric") {
+			x = sprintf("%.12f", x)
+			
+			x[is.na(x)] = "NaN"
+			x[x == "Infinity"]= "Infinity"
+			x[x == "-Infinity"]= "-Infinity"	
+		} else {
+			x = paste("\"", x , "\"", sep="")
+		}
+		x
+	}
+	
+	vAttrNames = list.vertex.attributes(g)
+	vAttrNames = vAttrNames[vAttrNames != "id"]
+	vAttr = lapply(vAttrNames, function(x) attrToString(get.vertex.attribute(g, x)))
+	names(vAttr) = vAttrNames
+	eAttrNames = list.edge.attributes(g)
+	eAttrNames = eAttrNames[eAttrNames != "id"]
+	eAttr = lapply(eAttrNames, function(x) attrToString(get.edge.attribute(g, x)))
+	names(eAttr) = eAttrNames
+	
+	f = file(fileName, "w")
+	cat("graph\n[", file=f)
+	cat(" directed ", as.integer(is.directed(g)), "\n", file=f)
+	for(i in seq_len(vcount(g))) {
+		cat(" node\n [\n", file=f)
+		cat("    id", i-1, "\n", file=f)
+		for(n in names(vAttr)) {
+			cat("   ", gsub("[\\._]", "", n), vAttr[[n]][i], "\n", file=f)
+		}
+		cat(" ]\n", file=f)
+	}
+	
+	el = get.edgelist(g, names=FALSE)
+	for (i in seq_len(nrow(el))) { 
+		cat(" edge\n  [\n", file=f) 
+		cat("  source", el[i,1], "\n", file=f) 
+		cat("  target", el[i,2], "\n", file=f) 
+		for(n in names(eAttr)) {
+			cat("   ", gsub("[\\._]", "", n), eAttr[[n]][i], "\n", file=f)
+		}
+		cat(" ]\n", file=f) 
+	}
+	
+	cat("]\n", file=f)
+	cat("Title \"", title, '"', file=f, sep="")
+	close(f)
+}
+
+################################################################
+## Combine undirected interaction networks into a             ##
+## single graph. Attributes will be merged when possible.     ##
+################################################################
+mergeGraphs = function(graphs, setSourceAttr = T, firstAsBase = F) {
+  interactions = graph.empty()
+  if(firstAsBase) {
+    interactions = graphs[[1]]
+    graphs = graphs[2:length(graphs)]
+    vas = list.vertex.attributes(interactions)
+    if(!("name" %in% vas) && "identifier" %in% vas) V(interactions)$name = V(interactions)$identifier
+  }
+  
+  for(graphName in names(graphs)) {
+    message("processing ", graphName)
+    g = graphs[[graphName]]
+    vas = list.vertex.attributes(g)
+    if(!("name" %in% vas) && "identifier" %in% vas) {
+      print("Setting identifier attribute as name")
+      V(g)$name = V(g)$identifier
+    }
+    
+    #Add nodes
+    newv = V(g)[!(name %in% V(interactions)$name)]
+    newnames = V(g)[newv]$name
+    attr = list()
+    attr$name = newnames
+    
+    interactions = add.vertices(
+      interactions, length(newv), attr = attr
+      )
+    
+    ## Merge node attributes
+    newAttr = setdiff(list.vertex.attributes(g), list.vertex.attributes(interactions))
+    for(an in newAttr) {
+      interactions = set.vertex.attribute(interactions, an, V(interactions), "")
+      interactions = set.vertex.attribute(interactions, an, V(interactions)[V(g)$name], get.vertex.attribute(g, an))
+    }
+    mergeAttr = intersect(list.vertex.attributes(g), list.vertex.attributes(interactions))
+    for(an in mergeAttr) {
+      if(length(newnames) > 0)
+        interactions = set.vertex.attribute(interactions, an, V(interactions)[newnames], get.vertex.attribute(g, an, V(g)[newnames]))
+      
+      ovl = setdiff(V(g)$name, newnames)
+      if(length(ovl) > 0) {
+        ovl.attr = cbind(
+          get.vertex.attribute(interactions, an, V(interactions)[ovl]),
+          get.vertex.attribute(g, an, V(g)[ovl])
+          )
+        ovl.attr = apply(ovl.attr, 1, function(x) {
+          xs = gsub("([\\.\\(\\)]{1})", "\\\\\\1", x[2])
+          if(is.na(x[1])) x[2]
+          else if(
+            x[1] != x[2] && 
+              length(grep(paste(xs, ", ", sep=""), x[1])) == 0 &&
+              length(grep(paste(xs, "$", sep=""), x[1])) == 0) {
+            paste(x, collapse = ", ")
+          } else {
+            x[1]
+          }
+        })
+        interactions = set.vertex.attribute(interactions, an, V(interactions)[ovl], ovl.attr)
+      }
+    }
+    
+    #Add edges
+    newNodeIndex = as.numeric(V(interactions)[V(g)$name])
+    edges = numeric(ecount(g)*2)
+    el = get.edgelist(g, names = F)
+    for(n in 1:nrow(el)) {
+      if(n %% 1000 == 0) message(n, " out of ", ecount(g))
+      edges[c(n*2-1,n*2)] = c(newNodeIndex[el[n,1]+1], newNodeIndex[el[n,2]+1])
+    }
+    attr = list()
+    for(n in list.edge.attributes(g)) {
+      attr[[n]] = as.character(get.edge.attribute(g, n))
+    }
+    if(setSourceAttr) attr$SourceFile = rep(graphName, ecount(g))
+    attr$sourceEdge = as.numeric(E(g))
+    interactions = add.edges(interactions, edges, attr = attr)
+  }
+  interactions
+}
+
+##################################################
+## Transfer data matrix to node/edge attributes ##
+##################################################
+dataToGraph = function(graph, data, cols, matchCol, edges = T, nodes = T, edge.function = max, edge.na = 1) {
+  for(col in cols) {
+    message("Processing ", col)
+    
+    if(nodes) {
+      nodeWeights = sapply(V(graph)$name, function(x) {
+        w = data[data[,matchCol] == x, col]
+        w = mean(w, na.rm=T)
+        if(is.nan(w)) w = NA
+        w
+      })
+      graph = set.vertex.attribute(graph, col, V(graph), nodeWeights)
+    }
+    
+    if(edges) {
+      nodeWeights = get.vertex.attribute(graph, col)
+      
+      edgeWeights = sapply(E(graph), function(e) {
+        edge = get.edge(graph, e)
+        v1 = nodeWeights[edge[1] + 1]
+        v2 = nodeWeights[edge[2] + 1]
+        if(is.na(v1)) v1 = edge.na
+        if(is.na(v2)) v2 = edge.na
+        edge.function(v1, v2)
+      })
+      graph = set.edge.attribute(graph, col, E(graph), edgeWeights)
+    }
+  }
+  graph
+}
+
+#########################################
+## Remove unconnected nodes from graph ##
+#########################################
+removeLonelyNodes = function(g) {
+  subgraph(g, which(igraph::degree(g) != 0) - 1)
+}
---
+++ network-analysis/wgcna.functions.R
@@ -0,0 +1,102 @@
+#################################################################
+## Functions for co-expression analysis with the WGCNA package ##
+#################################################################
+
+plotTraitMod = function(datTraits, trait, MEs, outPath, group, mod, r = "", title = "", xlab = mod, ylab = trait) {
+	library(ggplot2)
+	x = datTraits[,trait]
+	y = MEs[,mod]
+	dta = data.frame(
+		mouse = rownames(datTraits), trait = x, eg = y, group = group
+		)
+	
+	p = ggplot(dta, aes(eg, trait))
+	p = p + geom_point(aes(colour = group), size = 4)
+	#p = p + geom_text(aes(label = mouse), hjust=0, vjust=0, size=3)
+	p = p + opts(title = title)
+	p = p + xlab(xlab) + ylab(ylab)
+	
+	pdf(paste(outPath, "cor.", r, ".", trait, ".", mod, ".pdf", sep=""))
+	print(p)
+	dev.off()
+}
+
+plotTraitCorrelations = function(moduleTraitCor, moduleTraitPvalue, MEs, datTraits, file = NULL, plotPvalue = T, windowSize = c(10,6)) {
+  sizeGrWindow(windowSize[1], windowSize[2])
+  textMatrix = signif(moduleTraitCor, 2)
+  if(plotPvalue) {
+  	textMatrix = paste(textMatrix, "\n(",
+  	  signif(moduleTraitPvalue, 1), ")", sep = "")
+  }
+  dim(textMatrix) = dim(moduleTraitCor)
+  par(mar = c(6, 8.5, 3, 3))
+  labeledHeatmap(Matrix = moduleTraitCor,
+    xLabels = colnames(datTraits),
+    yLabels = names(MEs),
+    ySymbols = names(MEs),
+    colorLabels = FALSE,
+    colors = greenWhiteRed(50),
+    textMatrix = textMatrix,
+    setStdMargins = FALSE,
+    cex.text = 0.5,
+    zlim = c(-1,1),
+    main = paste("Module-trait relationships"))
+  
+  if(!is.null(file)) dev.copy2pdf(file = file)
+}
+
+plotSoftThreshold = function(sft, rs, file = NULL) {
+  ## Evaluate soft threshold
+  sizeGrWindow(9, 5)
+  par(mfrow = c(1,2));
+  cex1 = 0.9;
+  plot(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
+    xlab="Soft Threshold (power)",ylab="Scale Free Topology Model Fit,signed R^2",type="n",
+    main = paste("Scale independence"))
+    text(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
+    labels=powers,cex=cex1,col="red"
+  )
+  abline(h=rs,col="red")
+    plot(sft$fitIndices[,1], sft$fitIndices[,5],
+    xlab="Soft Threshold (power)",ylab="Mean Connectivity", type="n",
+    main = paste("Mean connectivity"))
+    text(sft$fitIndices[,1], sft$fitIndices[,5], labels=powers, cex=cex1,col="red"
+  )
+  
+  if(!is.null(file)) dev.copy2pdf(file = file)
+}
+
+
+plotHardThreshold = function(hrd, rs, cutoffs, file = NULL) {
+  ## Evaluate soft threshold
+  sizeGrWindow(9, 5)
+  par(mfrow = c(1,2));
+  cex1 = 0.9;
+  plot(hrd$fitIndices[,1], -sign(hrd$fitIndices[,4])*hrd$fitIndices[,5],
+       xlab="Hard Threshold (TOM)",ylab="Scale Free Topology Model Fit,signed R^2",type="n",
+       main = paste("Scale independence"))
+  text(hrd$fitIndices[,1], -sign(hrd$fitIndices[,4])*hrd$fitIndices[,5],
+       labels=cutoffs,cex=cex1,col="red"
+       )
+  abline(h=rs,col="red")
+  plot(hrd$fitIndices[,1], hrd$fitIndices[,6],
+       xlab="Hard Threshold (TOM)",ylab="Mean Connectivity", type="n",
+       main = paste("Mean connectivity"))
+  text(hrd$fitIndices[,1], hrd$fitIndices[,6], labels=cutoffs, cex=cex1,col="red"
+       )
+  
+  if(!is.null(file)) dev.copy2pdf(file = file)
+}
+
+plotNetAsDendro = function(net, file = NULL) {
+  sizeGrWindow(12, 9)
+  # Convert labels to colors for plotting
+  mergedColors = labels2colors(net$colors)
+  # Plot the dendrogram and the module colors underneath
+  plotDendroAndColors(net$dendrograms[[1]], mergedColors[net$blockGenes[[1]]],
+      "Module colors",
+      dendroLabels = FALSE, hang = 0.03,
+      addGuide = TRUE, guideHang = 0.05)
+  
+  if(!is.null(file)) dev.copy2pdf(file = file)
+}
