85d3ae6273c7c33d4788e0436d4a0a325a62446b
Thomas Kelder <thomaskelder@gmail.com>
2012-09-06 14:13:35 +0200

    tprofiler utility functions

---
+++ util/tprofiler.utils.R
@@ -0,0 +1,33 @@
+######################################
+## Utility functions for T-profiler ##
+######################################
+require(pheatmap)
+	
+tprofilerHeatmap = function(
+	gctFile, minSigSamples = 3, cutoffT = 4, minmax = 5, 
+	colors = c('blue', 'white', 'red'), modifyColnames = function(x) x,
+	cellwidth = 8, cellheight = 8, 
+	clustering_distance_rows="correlation", clustering_distance_cols="correlation",
+	...
+) {
+	
+	tprof = read.delim(gctFile, as.is=T, skip = 3)
+	rownames(tprof) = tprof[,1]
+	tprof = tprof[,3:ncol(tprof)]
+
+	tprof.filt = tprof[rowSums(abs(tprof) > cutoffT) >= minSigSamples,]
+
+	hdata = tprof.filt
+
+	colnames(hdata) = modifyColnames(colnames(hdata))
+	nc = 256
+	colors = colorRampPalette(colors)(nc+2)
+	breaks = seq(-minmax, minmax, (2*minmax)/(nc-1))
+	breaks = c(min(hdata, na.rm=T)-1, breaks, max(hdata, na.rm=T)+1)
+	pheatmap(
+	  hdata, color = colors, scale = "none", breaks = breaks,
+	  cellwidth = cellwidth, cellheight = cellheight, 
+	  clustering_distance_rows=clustering_distance_rows, clustering_distance_cols=clustering_distance_cols,
+	  ...
+	)
+}
