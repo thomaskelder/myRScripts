c772e933c4ed562cad39b23279f30d4eb3154422
Thomas Kelder <thomaskelder@gmail.com>
2012-09-21 10:40:32 +0200

    tprofiler heatmap: added option to cluster per group

--- util/tprofiler.utils.R
+++ util/tprofiler.utils.R
@@ -4,11 +4,11 @@
 require(pheatmap)
 	
 tprofilerHeatmap = function(
-  gctFile, minSigSamples = 3, cutoffT = 4, minmax = 5, parseGroupsForSigSamples = NULL,
+  gctFile, minSigSamples = 3, cutoffT = 4, minmax = 5,
   colors = c('blue', 'white', 'red'), modifyColnames = function(x) x,
   cellwidth = 8, cellheight = 8, selectCols = function(x) x,
   clustering_distance_rows="correlation", clustering_distance_cols="correlation",
-  ...
+  parseGroups = NULL, parseGroupsForSigSamples = parseGroups, clusterPerGroup = F, cluster_cols = T, colDist = function(x) as.dist(1 - cor(x)), colClustMeth = "complete", ...
 ) {
   
   tprof = read.delim(gctFile, as.is=T, skip = 3)
@@ -28,6 +28,23 @@ tprofilerHeatmap = function(
   }
   
   hdata = tprof.filt
+
+  if(clusterPerGroup & !is.null(parseGroups)) {
+   groups = parseGroups(colnames(hdata))
+  
+	matlist = lapply(unique(groups), function(g) {
+		cols = colnames(hdata)[groups == g]
+		colorder = hclust(colDist(hdata[,cols]), method=colClustMeth)
+		cbind(hdata[, cols[colorder$order]], matrix(0, nrow = nrow(hdata), ncol = 1))
+	})
+	mat = matlist[[1]]
+	for(m in 2:length(matlist)) mat = cbind(mat, matlist[[m]])
+	mat = as.matrix(mat)
+   colnames(mat)[grep("^matrix\\(", colnames(mat))] = ""
+   mat = mat[, 1:(ncol(mat)-1)]
+	hdata = mat
+	cluster_cols = F
+  }
   
   colnames(hdata) = modifyColnames(colnames(hdata))
   nc = 256
@@ -38,6 +55,6 @@ tprofilerHeatmap = function(
     hdata, color = colors, scale = "none", breaks = breaks,
     cellwidth = cellwidth, cellheight = cellheight, 
     clustering_distance_rows=clustering_distance_rows, clustering_distance_cols=clustering_distance_cols,
-    ...
+    cluster_cols = cluster_cols, ...
   )
 }
