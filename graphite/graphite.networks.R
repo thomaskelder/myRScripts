a33a8b79a6073d288119796b00192aaa774bb136
Thomas Kelder <thomaskelder@gmail.com>
2012-11-02 14:19:28 +0100

    added script to create graphs from the graphite package

---
+++ graphite/graphite.networks.R
@@ -0,0 +1,101 @@
+#########################################################
+## Methods to deal with networks from graphite package ##
+#########################################################
+pathwayToSpecies = function(pathway, homology, outProt) {
+  library("org.Hs.eg.db")
+  
+  pathwayEntrez = convertIdentifiers(pathway, 'entrez')
+
+  ## Find homologs for human ids in pathway
+  entrezHuman = nodes(pathwayEntrez)
+  human2hom = lapply(entrezHuman, mapGeneByHomology, homology = homology, outProt = outProt, inProt = org.Hs.egENSEMBLPROT)
+  names(human2hom) = entrezHuman
+  human2hom[is.null(human2hom)] = c()
+  
+  ## Convert edges to homologs
+  edgesEntrez = edges(pathwayEntrez)
+  edgesHom = edgesEntrez[F, , drop=F]
+  for(i in seq_len(nrow(edgesEntrez))) {
+    hrow = edgesEntrez[i,]
+    h1 = as.character(hrow[1])
+    h2 = as.character(hrow[2])
+    for(x1 in human2hom[[h1]]) {
+      for(x2 in human2hom[[h2]]) {
+        xrow = hrow
+        xrow[1] = x1
+        xrow[2] = x2
+        edgesHom = rbind(edgesHom, xrow)
+      }
+    }
+  }
+  
+  pathwayHom = pathwayEntrez
+  pathwayHom@nodes = as.character(unlist(human2hom))
+  pathwayHom@edges = edgesHom
+  pathwayHom
+}
+
+mapGeneByHomology = function(g, homology, outProt, inProt) {
+  hp = unlist(mget(g, inProt, ifnotfound=NA))
+  hp = hp[!is.na(hp)]
+  if(length(hp) == 0) return(c())
+  mp = unlist(mget(hp, homology, ifnotfound=NA))
+  mp = mp[!is.na(mp)]
+  if(length(mp) == 0) return(c())
+  me = unlist(mget(mp, outProt, ifnotfound=NA))
+  me = me[!is.na(me)]
+  unique(me)
+}
+
+pathwaysToIGraph = function(pathways) {
+  library(devtools)
+  source_url("https://raw.github.com/thomaskelder/myRScripts/master/network-analysis/igraph.functions.R")
+  
+  graph = graph.empty(directed = T)
+  for(pn in names(pathways)) {
+    p = pathways[[pn]]
+    d = graphite::edges(p)
+    if(nrow(d) == 0) next
+    
+    ## Double edges for undirected
+    d = rbind(d, d[d$direction == 'undirected', c(2, 1, 3:ncol(d))])
+    
+    pgraph = graph.data.frame(d)
+    m = list(graph)
+    m[[pn]] = pgraph
+    graph = mergeGraphs(m, firstAsBase=T)
+  }
+  
+  graph
+}
+
+runGraphiteSPIA = function(pathways, de, all) {
+  
+}
+
+runSPIA <- function(de, all, pathwaySetName, ...) {
+  if (!require(SPIA))
+    stop("library SPIA is missing")
+  
+  checkPkgVersion("SPIA", "2.2")
+  
+  if (!(datasetName(pathwaySetName) %in% dir()))
+    stop("There is no dataset corresponding to the pathway set name: ", pathwaySetName, "\n",
+         "Did you forget to run prepareSPIA?")
+  
+  optArgs <- list(...)
+  if (!is.null(optArgs$organism))
+    warning("Ignoring the \"organism\" parameter.")
+  optArgs$organism <- pathwaySetName
+  
+  spiaEnv <- environment(spia)
+  fakeEnv <- new.env(parent=spiaEnv)
+  assign("system.file", fakeSystemFile, fakeEnv)
+  
+  tryCatch({
+    environment(spia) <- fakeEnv
+    do.call(spia, c(list(de, all), optArgs))[,c(-2,-12)]
+  }, finally={
+    environment(spia) <- spiaEnv
+  })
+}
---
+++ graphite/graphite.tospecies.R
@@ -0,0 +1,44 @@
+################################################
+## Convert graphite pathways to other species ##
+################################################
+
+library(foreach)
+library(doMC)
+registerDoMC(cores = 25)
+
+source("graphite.networks.R")
+
+## Assemble pathway list in other R process, because
+## RSQLite has problems with next (parallel) step if
+## already loaded in parent process.
+if(!file.exists("graphitePathways.RData")) {
+  message("Preparing graphite pathways list")
+  library(graphite)
+  appendPathways = function(l, p, name) {
+    names(p) = paste0(names(p), " (", name, ")")
+    append(l, p)
+  }
+  graphitePathways = biocarta
+  names(graphitePathways) = paste(names(biocarta), "(biocarta)")
+  graphitePathways = appendPathways(graphitePathways, kegg, "kegg")
+  graphitePathways = appendPathways(graphitePathways, reactome, "reactome")
+  graphitePathways = appendPathways(graphitePathways, nci, "nci")
+  save(graphitePathways, file = "graphitePathways.RData")
+} else {
+  message("Loading graphite pathways list")
+  load("graphitePathways.RData")
+}
+
+graphiteMouse = foreach(p=graphitePathways , .verbose = T) %dopar% {
+  library(graphite)
+  library(hom.Hs.inp.db)
+  library(org.Mm.eg.db)
+  library(org.Hs.eg.db)
+  pathwayToSpecies(p, homology = hom.Hs.inpMUSMU, outProt = org.Mm.egENSEMBLPROT2EG)
+}
+names(graphiteMouse) = names(graphitePathways)
+save(graphiteMouse, file = "graphitePathwaysMouse.RData");
+
+graphiteMouseGraph = pathwaysToIGraph(graphiteMouse)
+save(graphiteMouseGraph, file = "graphiteGraphMouse.RData")
+saveGML(graphiteMouseGraph, fileName = "graphiteGraphMouse.gml", "Graphite-mouse")
