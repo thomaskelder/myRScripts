6c09194a3a0bcfc78b0e86fd031162258bc8878f
Thomas Kelder <thomaskelder@gmail.com>
2012-11-13 11:50:36 +0100

    Update network-analysis/wgcna.functions.R

--- network-analysis/wgcna.functions.R
+++ network-analysis/wgcna.functions.R
@@ -21,26 +21,29 @@ plotTraitMod = function(datTraits, trait, MEs, outPath, group, mod, r = "", titl
 	dev.off()
 }
 
-plotTraitCorrelations = function(moduleTraitCor, moduleTraitPvalue, MEs, datTraits, file = NULL, plotPvalue = T, windowSize = c(10,6), colors = greenWhiteRed(50)) {
+plotTraitCorrelations = function(moduleTraitCor, moduleTraitPvalue, MEs, datTraits, file = NULL, plotPvalue = T, windowSize = c(10,6), colors = greenWhiteRed(50), plotR = T) {
   sizeGrWindow(windowSize[1], windowSize[2])
-  textMatrix = signif(moduleTraitCor, 2)
-  if(plotPvalue) {
-  	textMatrix = paste(textMatrix, "\n(",
-  	  signif(moduleTraitPvalue, 1), ")", sep = "")
+  textMatrix = NULL
+  if(plotR) {
+    textMatrix = signif(moduleTraitCor, 2)
   }
-  dim(textMatrix) = dim(moduleTraitCor)
+  if(plotPvalue) {
+    textMatrix = paste(textMatrix, "\n(",
+                       signif(moduleTraitPvalue, 1), ")", sep = "")
+  }
+  if(!is.null(textMatrix)) dim(textMatrix) = dim(moduleTraitCor)
   par(mar = c(6, 8.5, 3, 3))
   labeledHeatmap(Matrix = moduleTraitCor,
-    xLabels = colnames(datTraits),
-    yLabels = names(MEs),
-    ySymbols = names(MEs),
-    colorLabels = FALSE,
-    colors = colors,
-    textMatrix = textMatrix,
-    setStdMargins = FALSE,
-    cex.text = 0.5,
-    zlim = c(-1,1),
-    main = paste("Module-trait relationships"))
+                 xLabels = colnames(datTraits),
+                 yLabels = names(MEs),
+                 ySymbols = names(MEs),
+                 colorLabels = FALSE,
+                 colors = colors,
+                 textMatrix = textMatrix,
+                 setStdMargins = FALSE,
+                 cex.text = 0.5,
+                 zlim = c(-1,1),
+                 main = paste("Module-trait relationships"))
   
   if(!is.null(file)) dev.copy2pdf(file = file)
 }
